715869a19cca7d3060320a618e2ba0341d25da35
Vera Var <primavera1919@yandex.ru>
2020-07-06 14:14:28 +0300

    Test data and tests changed

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,37 +7,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
 
   StatisticsService service = new StatisticsService();
-  long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+  long[] incomesInBillions = {1, 2, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
 
   @Test
   void shouldFindMax(){
     long expected = 12;
-
     long actual = service.findMax(incomesInBillions);
     assertEquals(expected, actual);
   }
 
   @Test
   void shouldFindMin(){
-    long expected = 3;
-
+    long expected = 1;
     long actual = service.findMin(incomesInBillions);
     assertEquals(expected, actual);
   }
 
   @Test
   void shouldFindSum(){
-    long expected = 85;
-
+    long expected = 76;
     long actual = service.findSum(incomesInBillions);
     assertEquals(expected, actual);
   }
 
   @Test
   void shouldFindAverage(){
-    long expected = 7;
+    long expected = 6;
     long actual = service.findAverage(incomesInBillions);
-
     assertEquals(expected, actual);
   }
 }
